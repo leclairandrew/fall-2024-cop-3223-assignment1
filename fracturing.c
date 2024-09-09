142113a6e240e371624a5391719699cfef00a2ec
leclairandrew <an237991@ucf.edu>
2024-09-09 05:50:30 +0000

    test

---
+++ fracturing.c
@@ -0,0 +1,44 @@
+#include <math.h>
+#include <stdio.h>
+
+double calculateDistance();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth():
+double calculateHeight();
+
+int main(int argc, char **argv){
+
+    double calculateDistance();
+    double calculatePerimeter();
+    double calculateArea();
+    double calculateWidth():
+    double calculateHeight();
+
+    return 0;
+}
+
+double calculateDistance(){
+
+    return result
+}
+
+double calculatePerimeter(){
+
+    return 
+}
+
+double calculateArea(){
+
+    return
+}
+
+double calculateWidth(){
+
+    return
+}
+
+double calculateHeight(){
+
+    return
+}
